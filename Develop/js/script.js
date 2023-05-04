e4804f1063fd2df734b2b1ae385092571af13fbf
k-pineda <karenp9310@gmail.com>
2023-05-04 18:31:37 -0400

    added api key & starter code to build geocode

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -9,12 +9,25 @@ var weatherTemperature=$("#temp");
 var weatherWind=$("#wind");
 var weatherHumidity=$("#humidity");
 
-
-
-var currentDay = dayjs().format("MM/DD/YYYY");
+var currentDay = dayjs().format("(MM/DD/YYYY)");
 $("#currentDay").text(currentDay);
 
-var requestWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}id=3484e08d51e803d19133758ad6e77ac5&appid={API key}}';
+
+// step 1 fetch geo location and save data to varibale
+// step 2 plug in variable name into template literal string
+
+// var geolocation = fetch("geo location")
+
+var geoLocation = cityUserInput.value
+
+var requestGeocodeUrl = `https://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid=3484e08d51e803d19133758ad6e77ac5`;
+
+
+var requestWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geolocation}&lon=${geolocation}&appid=3484e08d51e803d19133758ad6e77ac5`;
+var myName = "Allan"
+var templateLiteralExample = `Hi my name is ${myName}`
+
+console.log(templateLiteralExample);
 
 function getApi(requestUrl) {
     fetch(requestUrl)
