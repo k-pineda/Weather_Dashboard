a2c70884301c6661b4d034ecffaf26ca7c5ed2c8
k-pineda <karenp9310@gmail.com>
2023-05-04 17:04:31 -0400

    selected variables

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,3 +1,19 @@
+var searchHistory=$("#search-history");
+var cityUserInput=$(".city-input");
+var citySearchBtn=$(".search");
+var requestedWeather=$(".weather-display");
+var weatherHeaderEl=$(".weather-header");
+var futureForecast=$(".future-forecast");
+var currentForecast=$("current-forecast");
+var weatherTemperature=$("#temp");
+var weatherWind=$("#wind");
+var weatherHumidity=$("#humidity");
+
+
+
+var currentDay = dayjs().format("MM/DD/YYYY");
+$("#currentDay").text(currentDay);
+
 var requestWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}id=3484e08d51e803d19133758ad6e77ac5&appid={API key}}';
 
 function getApi(requestUrl) {
